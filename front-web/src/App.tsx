ec3684ba49521c8d62bca83afe08a15d0c23b1aa
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-07 20:53:52 -0300

    Add Navbar

--- front-web/src/App.tsx
+++ front-web/src/App.tsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import './App.css';
-import Hello from './Hello';
+import Navbar from './Navbar';
 
 function App() {
   return (
     <div >
-      <h1>Hello World!</h1>
+      <Navbar />
     </div>
-  );
+  )
 }
 
 export default App;
