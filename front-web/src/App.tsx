fbee20197e5ff8a86167cd03e34c3e17ae4d5f21
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-07 20:20:48 -0300

    Create front-web initial files

---
+++ front-web/src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import Hello from './Hello';
+
+function App() {
+  return (
+    <div >
+      <h1>Hello World!</h1>
+    </div>
+  );
+}
+
+export default App;
