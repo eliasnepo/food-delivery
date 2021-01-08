f987f68921e72d4b7c74ea34cec16c15eb0f6d84
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-07 23:26:25 -0300

    Add Routes and StepsHeader

--- front-web/src/Footer/index.tsx
+++ front-web/src/Footer/index.tsx
@@ -5,7 +5,7 @@ import { ReactComponent as InstagramIcon} from './Instagram.svg';
 function Footer() {
     return (
         <footer className="main-footer">
-            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior.
+            App desenvolvido durante a 2ª ed. do evento <strong>Semana DevSuperior 2.0</strong>
             <div className="footer-icons">
                 <a href="https://www.linkedin.com/in/elias-nepomuceno-232273192/" target="_new"> <LinkedinIcon /> </a>
                 <a href="https://www.instagram.com/eliasnepo/" target="_new"> <InstagramIcon /> </a>
--- front-web/src/Home/index.tsx
+++ front-web/src/Home/index.tsx
@@ -1,5 +1,6 @@
 import './styles.css';
 import { ReactComponent as MainImage } from './main.svg';
+import { Link } from 'react-router-dom';
 import Footer from '../Footer';
 
 function Home() {
@@ -10,7 +11,7 @@ function Home() {
                     <div className="home-actions">
                         <h1 className="home-title">Faça seu pedido <br /> que entregamos <br /> para você!!!</h1>
                         <h3 className="home-subtitle">Escolha o seu pedido e em poucos minutos <br /> levaremos na sua porta</h3>
-                        <a className="home-btn-order" href="orders">FAZER PEDIDO</a>
+                        <Link to="/orders" className="home-btn-order">FAZER PEDIDO</Link>
                     </div>
                     <div className="home-image">
                         <MainImage />
