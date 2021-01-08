325bb3b922a8087b6d57e8a20f8085009b4c52a3
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-07 21:22:12 -0300

    Add Footer

---
+++ front-web/src/Footer/index.tsx
@@ -0,0 +1,17 @@
+import './styles.css';
+import { ReactComponent as LinkedinIcon} from './Linkedin.svg';
+import { ReactComponent as InstagramIcon} from './Instagram.svg';
+
+function Footer() {
+    return (
+        <footer className="main-footer">
+            App desenvolvido durante a 2ª ed. do evento Semana DevSuperior.
+            <div className="footer-icons">
+                <a href="https://www.linkedin.com/in/elias-nepomuceno-232273192/" target="_new"> <LinkedinIcon /> </a>
+                <a href="https://www.instagram.com/eliasnepo/" target="_new"> <InstagramIcon /> </a>
+            </div>
+        </footer>
+    )
+}
+
+export default Footer;
