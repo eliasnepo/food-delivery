a0f26e2284c98f017d28762f6e38d9d272827d04
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-12 13:08:35 -0300

    Add home page

---
+++ front-mobile/src/Header/index.tsx
@@ -0,0 +1,31 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View, Image } from 'react-native';
+
+export default function Header() {
+  return (
+    <View style={styles.container}>
+        <Image source={require('../assets/logo.png')} />
+        <Text style={styles.text}>DS Delivery</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: '#DA5C5C',
+    height: 75,
+    paddingTop: 35,
+    flexDirection: 'row',
+    justifyContent: 'center'
+  },
+  text: {
+    fontWeight: 'bold',
+    fontSize: 18,
+    lineHeight: 25,
+    letterSpacing: -0.24,
+    color: '#FFF',
+    marginLeft: 15,
+    fontFamily: 'OpenSans_700Bold'
+  },
+});
