4817382f61ada5cf4c3c68f048678f064178e5aa
Elias Nepomuceno <eliasnepo12@gmail.com>
2021-01-12 14:58:30 -0300

    Final project mobile

--- front-mobile/src/OrderCard/index.tsx
+++ front-mobile/src/OrderCard/index.tsx
@@ -1,26 +1,51 @@
 import React from 'react';
+import Intl from "intl";
+import 'intl/locale-data/jsonp/pt-BR'
 import { StyleSheet, Text, View, Image } from 'react-native';
-import { RectButton } from 'react-native-gesture-handler';
-import Header from '../Header';
+import { Order } from '../types';
+import dayjs from 'dayjs';
+import 'dayjs/locale/pt-br';
+import relativeTime from 'dayjs/plugin/relativeTime';
 
-function OrderCard() {
+dayjs.locale('pt-br');
+dayjs.extend(relativeTime);
+
+type Props = {
+    order: Order;
+}
+
+function dateFromNow(date: string) {
+    return dayjs(date).fromNow();
+}
+
+export function formatPrice(price: number) {
+    const formatter = new Intl.NumberFormat('pt-BR', {
+      style: 'currency',
+      currency: 'BRL',
+      minimumFractionDigits: 2,
+    });
+  
+    return formatter.format(price);
+}
+
+function OrderCard({ order } : Props) {
   return (
     <View style={styles.container}>
         <View style={styles.header}>
             <Text style={styles.orderName}>
-                Pedido 1
+                Pedido {order.id}
             </Text>
             <Text style={styles.orderPrice}>
-                R$ 50,00
+                {formatPrice(order.total)}
             </Text>
         </View>
         <Text style={styles.text}>
-            Há 30 min
+            {dateFromNow(order.moment)}
         </Text>
         <View style={styles.productsList}>
-        <Text style={styles.text}>Pizza Calabresa</Text>
-        <Text style={styles.text}>Pizza Quatro Queijos</Text>
-        <Text style={styles.text}>Pizza Mussarela</Text>
+            {order.products.map(product => (
+                <Text key={product.id} style={styles.text}>{product.name}</Text>
+            ))}
         </View>
     </View>
   );
